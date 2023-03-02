a4337b011607f63eddc1d3fbd87e7aca81124e60
phleech <phleech@phleech.co.uk>
2023-03-02 20:48:34 +0000

    Initial commit.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Docker PHP Template</title>
+        <title>phleech</title>
+    <style>
+        p {
+            font-family: "Times New Roman", Times, serif;
+            font-size:16px;
+            margin:0px;
+            padding:0px;
+            text-align:center;
+            width:100%;
+        }
+        div {
+            position:absolute;
+            left:0px;
+            top:40%;
+            width:100%;
+        }
+    </style>
     </head>
     <body>
         <div>
-            <p>Congrats, she's up.</p>
+            <p><strong>Fleeches (FL-EE-CHUH)</strong> are large, greenish leech-like creatures found in Oddworld’s swamps, jungles, underground vaults, and sewers.<br/ >Abe encounters Fleeches during his travels through Necrum and the Soulstorm Brewery Kennels.</p>
         </div>
     </body>
 </html>
