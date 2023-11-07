220c30361624a9337c85915bd6598379db37c026
phleech <phleech@phleech.co.uk>
2023-11-07 23:01:04 +0000

    Adding social icons

--- public/index.php
+++ public/index.php
@@ -2,7 +2,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title>phleech</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
         <style type="text/css">
+            :root {
+                --primary-color: #f368e0;
+            }
             @font-face {
                 font-family: "VarelaRound-Regular";
                 src: url("VarelaRound-Regular.ttf"), url("VarelaRound-Regular.ttf");
@@ -10,31 +14,42 @@
             body {
                 background-image: url(pattern.png);
                 background-repeat: repeat;
-                color: #f368e0;
+                color: var(--primary-color);
                 font-family: VarelaRound-Regular;
             }
             .container {
-                position:absolute;
-                left:0px;
-                top:40%;
-                width:100%;
+                position: absolute;
+                left: 0px;
+                top: 40%;
+                width: 100%;
             }
             .item {
-                margin:0px;
-                padding:0px;
-                text-align:center;
-                width:50%;
-                padding:0 25%;
+                margin: 0px;
+                padding: 0px;
+                text-align: center;
+                width: 50%;
+                padding: 0 25%;
             }
             h1 {
-                font-size:30px;
+                font-size: 30px;
+                margin-top: 0;
+                margin-bottom: 30px;
             }
             h2 {
-                font-size:15px;
+                font-size: 15px;
             }
             a:link, a:visited, a:hover, a:active {
-                color: #fafafa;
                 text-decoration: none;
+            }
+            a:link, a:visited {
+                color: #fafafa;
+            }
+            a:hover, a:active {
+                color: var(--primary-color);
+            }
+            .btn {
+                margin: 10px;
+                font-size: 26px;
             }
         </style>
     </head>
@@ -42,7 +57,12 @@
         <div class="container">
             <div class="item">
                 <h1>Purchased one of my badges? Thank you!</h1>
-                <h2><a href="https://twitter.com/phl33ch">phleech</a></h2>
+                <a class="btn" href="https://github.com/phleech">
+                    <i class="fa-brands fa-github"></i>
+                </a>
+                <a class="btn" href="https://twitter.com/phl33ch">
+                    <i class="fa-brands fa-x-twitter"></i>
+                </a>
             </div>
         </div>
     </body>
